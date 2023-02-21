797d79e7cf74b6e093c9344ccdaacec683b27054
Stas Piskovski <stasila89@gmail.com>
2023-02-21 22:36:48 +0300

    random

--- Summ/Program.cs
+++ Summ/Program.cs
@@ -1,6 +1,6 @@
-﻿int numberA = new Random().Next(1,10);
+﻿int numberA = new Random().Next(1,100);
 Console.WriteLine(numberA);
 int numberB = new Random().Next(1,10);
 Console.WriteLine(numberB);
-int result = numberA + numberB;
+int result = numberA * numberB;
 Console.WriteLine(result);
